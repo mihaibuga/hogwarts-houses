0df9aea48d32f32213cdf4165477f1b638a2f548
mihaibuga <77620547+mihaibuga@users.noreply.github.com>
2021-11-25 12:54:29 +0200

    create Recipe model for db context

---
+++ Models/Entities/Recipe.cs
@@ -0,0 +1,17 @@
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace HogwartsPotions.Models.Entities
+{
+    public class Recipe
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public long ID { get; set; }
+
+        public string Name { get; set; }
+
+        public Student Student { get; set; }
+
+        public HashSet<Ingredient> Residents { get; set; } = new HashSet<Ingredient>();
+    }
+}
