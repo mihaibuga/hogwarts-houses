d05183c5486fcfd8891fe931039807319cc7f15a
mihaibuga <77620547+mihaibuga@users.noreply.github.com>
2021-11-25 22:51:53 +0200

    rename ingredients column in recipe model

--- Models/Entities/Recipe.cs
+++ Models/Entities/Recipe.cs
@@ -14,6 +14,6 @@ namespace HogwartsPotions.Models.Entities
 
         public Student Student { get; set; }
 
-        public HashSet<Ingredient> Residents { get; set; } = new HashSet<Ingredient>();
+        public HashSet<Ingredient> Ingredients { get; set; } = new HashSet<Ingredient>();
     }
 }
